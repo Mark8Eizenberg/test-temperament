4d970a78466af2e6a9a6de2c32cb3462c94884ae
Max Skorik <maksimskorik1999@gmail.com>
2023-01-18 14:20:10 +0200

    netlify adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,9 @@
 import adapter from '@sveltejs/adapter-netlify';
-
+const neylify = require('@sveltejs/adapter-netlify')
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: netlify()
+		adapter: neylify()
 	}
 };
 
